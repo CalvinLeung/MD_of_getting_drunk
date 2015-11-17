c67ebd0e0f4928629081b9be7684c7401680e5a8
Calvin Leung <cleung@hmc.edu>
2015-11-17 23:14:58 +0000

    blah

--- testPyfile.py
+++ testPyfile.py
@@ -1,18 +1,30 @@
 # Brownian Motion on the Unit Square
 # Parameters
-# a - wall spacing
-# m - effective particle mass
-# N - particle number
-# T - temperature (set k_B = 1)
-def genIC(m,N,T):
-
+# wallParameter - wall spacing
+# particleM - effective particle mass
+# NP - particle number
+# Temp - temperature which dictates thermal distribution (set k_B = 1)
+def genIC(particleM,NP,T):
+    particleX = np.random,rand([NP,2])
+    particleV = np.random.rand([NP,2])
     return (particleX, particleV)
 
-def plotState(particleX, particleV)
+def plotState(particleX, particleV):
     import matplotlib.pyplot as plt
     import numpy as np
     from numpy import ma
 
     plt.figure()
-
+    Q = plt.quiver(particleX,particleV)
     return 0
+
+
+def evolveTimeStep(particleX, particleV, wallParameter,Temp):
+    return particleV
+
+def main():
+    particleX, particleV = genIC(1,10,1)
+    plotState(particleX,particleV)
+
+if __name__ == "__main__":
+    main()
