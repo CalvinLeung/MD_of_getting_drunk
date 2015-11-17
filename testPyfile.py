b09ef692657455f84e24ad3db922ffbaf63fd86a
Calvin Leung <cleung@hmc.edu>
2015-11-17 18:34:59 +0000

    This is the Brownian motion simulation -CL

--- testPyfile.py
+++ testPyfile.py
@@ -1,1 +1,18 @@
-# PHYS 117 project Calvin Leung
+# Brownian Motion on the Unit Square
+# Parameters
+# a - wall spacing
+# m - effective particle mass
+# N - particle number
+# T - temperature (set k_B = 1)
+def genIC(m,N,T):
+
+    return (particleX, particleV)
+
+def plotState(particleX, particleV)
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from numpy import ma
+
+    plt.figure()
+
+    return 0
