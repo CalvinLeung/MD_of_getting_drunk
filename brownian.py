55a81b42b52b375c366ab682c969ec9f64f8dc35
Calvin Leung <cleung@hmc.edu>
2015-11-21 15:19:13 +0000

    fuck cs70

--- brownian.py
+++ brownian.py
@@ -10,12 +10,14 @@ import numpy as np
 from numpy import ma
 
 
-def genIC(particleM,NP,kB, Temp):
+def genIC(NP,kB, Temp,particleM):
     particleX = np.random.rand(NP,2)
     particleX[:,0] = 0.5 * particleX[:,0]
-    speed = np.random.normal(0,particleM/(2 * kB * Temp),(NP,1)) # Boltzmann factor for some speed
-    angle = np.random.rand(NP,1) * 2 * np.pi
-    particleV = np.hstack((np.multiply(speed,np.cos(angle)),np.multiply(speed,np.sin(angle))))
+    #speed = np.random.normal(0,particleM/(2 * kB * Temp),(NP,1)) # Boltzmann factor for some speed
+    #angle = np.random.rand(NP,1) * 2 * np.pi
+    #particleV = np.hstack((np.multiply(speed,np.cos(angle)),np.multiply(speed,np.sin(angle))))
+
+    particleV = np.random.normal(0,2*kB*Temp/particleM,(NP,2))
     return (particleX, particleV)
 
 def plotState(particleX, particleV):
@@ -24,15 +26,19 @@ def plotState(particleX, particleV):
     plt.show()
     return 0
 
-def totalEnergy(particleV,m,kb,Temp):
+def totalEnergy(particleV,m,kB,Temp):
     NP = particleV.shape[0]
-    T = particleM * sum(particleV[:,0] ** 2 + particleV[:,1] ** 2) / NP
+    T = 0.5 * particleM * sum(particleV[:,0] ** 2 + particleV[:,1] ** 2) / NP
     print('Observed average kinetic energy per particle (NP = ' + str(NP) + ') = ' +str(T))
-    print('Equipartition predicts 2 QDOF: ' + str(kb*Temp))
+    print('Equipartition predicts 2 QDOF: ' + str(kB*Temp))
+    
 def updateV(particleV, particleM, timeStep, gamma, kB, Temp):
-    zeta = np.random.normal(0,2*gamma*kB*Temp,(particleV.shape[0],1)) # Fluctuation Disspation Theorem
-    theta = 2*np.pi*np.random.rand(particleV.shape[0],1) #Random scattering angle
-    stoch = np.hstack((np.multiply(zeta,np.cos(theta)),np.multiply(zeta,np.sin(theta))))
+    #speed = np.sqrt(np.random.normal(0,2*gamma*kB*Temp,(particleV.shape[0],1))) # Fluctuation Disspation Theorem
+    #theta = 2*np.pi*np.random.rand(particleV.shape[0],1) #Random scattering angle
+    #stoch = np.hstack((np.multiply(zeta,np.cos(theta)),np.multiply(zeta,np.sin(theta))))
+    #vx = np.random.normal(0,2*gamma*kB*Temp,(particleV.shape[0],1))
+    #vy = np.random.normal(0,2*gamma*kB*Temp,(particleV.shape[0],1))
+    #stoch = np.hstack((vx,vy))
     stoch = 0
     dV = (-gamma * particleV + stoch)/particleM * timeStep
     return particleV + dV
@@ -43,8 +49,8 @@ def handleCollision(projectedX,particleV,wallParam):
     for i in range(projectedX.shape[0]):
         x,y = projectedX[i,:]
         xd,yd = particleV[i,:]
-        print('wall collision')
         while(x < 0 or x > 1 or y < 0 or y > 1):
+            print('wall collision')
             print('(x,y) = ' + str((x,y)) + ' being reflected...')
             if x < 0:
                 x = abs(x)
@@ -71,7 +77,7 @@ def randomWalk(totalSteps,timeStep,NP,Temp,particleM,particleR,eta,kB,wallParam)
     xHistory = np.zeros((NP,2,totalSteps))
     vHistory = np.zeros((NP,2,totalSteps))
     
-    particleX,particleV = genIC(particleM,NP,kB,Temp)
+    particleX,particleV = genIC(NP,kB,Temp,particleM)
     #print(particleX)
     #print(particleV)
     for i in range(0,totalSteps):
@@ -80,10 +86,12 @@ def randomWalk(totalSteps,timeStep,NP,Temp,particleM,particleR,eta,kB,wallParam)
         vHistory[:,:,i] = particleV[:,:]
         
         particleV = updateV(particleV,particleM,timeStep,gamma,kB,Temp)
-        print(particleV.max())
+        print(totalEnergy(particleV,particleM,kB,Temp))
+        input('Press enter')
         projectedX = particleX + particleV * timeStep
-        #print(projectedX)
-        particleX,particleV = handleCollision(projectedX,particleV, wallParam)
+        #Handle collisions
+        #particleX,particleV = handleCollision(projectedX,particleV, wallParam)
+        particleX = projectedX
         #input('Press Enter')
     return xHistory,vHistory
 
@@ -91,7 +99,7 @@ def randomWalk(totalSteps,timeStep,NP,Temp,particleM,particleR,eta,kB,wallParam)
 #plotState(particleX,particleV)
 kB = 1.4e-23 # Boltzmann's constant
 
-totalTime = 1e-9 # seconds of diffusion
+totalTime = 1e-8 # seconds of diffusion
 NP = 1
 Temp = 300 # 300 Kelvin
 particleM = 3 * 10 ** -26 # molecular mass of water in kg
@@ -101,8 +109,8 @@ wallParam = 0
 gamma = 6*np.pi*eta*particleR
 D = Temp * kB / gamma
 tau = particleR**2 / D
-timeStep = 100 * tau
-totalSteps = int(totalTime / (100 * tau))
+timeStep = tau
+totalSteps = 1000 #int(totalTime / timeStep)
 
 particleX, particleV = genIC(particleM,10,kB,Temp)
 
