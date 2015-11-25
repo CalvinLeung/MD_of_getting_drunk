7d43ff98b985e4e1ce90c5c366ec671d3e8b97f6
tmiddlemas <tmiddlemas@hmc.edu>
2015-11-24 22:51:23 -0800

    Got the brownian code working, it's too slow.
    The main slowdown is in the collision detction, we need a better algorithm.

--- brownian.py
+++ brownian.py
@@ -10,15 +10,15 @@ import numpy as np
 from numpy import ma
 
 
-def genIC(kB, NP, particleM, Temp):
-    particleX = np.random.rand(NP,3)
+def genIC(boxSize, kB, NP, particleM, Temp):
+    particleX = boxSize*np.random.rand(NP,3)
     particleX[:,0] = 0.5 * particleX[:,0]
     #speed = np.random.normal(0,particleM/(2 * kB * Temp),(NP,1)) # Boltzmann factor for some speed
     #angle = np.random.rand(NP,1) * 2 * np.pi
     #particleV = np.hstack((np.multiply(speed,np.cos(angle)),np.multiply(speed,np.sin(angle))))
 
-    #particleV = np.random.normal(0,np.sqrt(kB*Temp/particleM),(NP,2))
-    particleV = np.zeros((NP,3))
+    particleV = np.random.normal(0,np.sqrt(kB*Temp/particleM),(NP,3))
+    #particleV = np.zeros((NP,3))
     return (particleX, particleV)
 
 def plotState(particleX, particleV):
@@ -32,50 +32,53 @@ def totalEnergy(kB,particleM,particleV,Temp):
     NP = particleV.shape[0]
     T = 0.5 * particleM * sum(particleV[:,0] ** 2 + particleV[:,1] ** 2 + particleV[:,2] ** 2) / NP
     print('Observed average kinetic energy per particle (NP = ' + str(NP) + ') = ' +str(T))
-    print('Equipartition predicts 3 QDOF: ' + str(1.5 * kB *Temp))
+    print('Equipartition predicts (6-3 = 3) QDOF: ' + str(1.5 * kB *Temp))
     return T
     
 def updateV(gamma,kB,particleM,particleV,Temp,timeStep):
-    stoch = np.random.normal(0,np.sqrt(2*gamma*kB*Temp),(particleV.shape[0],3))
+    stoch = np.sqrt(24*kB*Temp*gamma/timeStep)*np.random.normal(0,np.sqrt(1.0/12.0),(particleV.shape[0],3))
     drag = -gamma * particleV
+    #print("stoch is" + str(stoch))
+    #print("drag is " + str(drag))
     #drag = 0
     #print(np.mean(stoch ** 2) / np.mean(drag ** 2))
-    dV = ((drag + stoch)/particleM)* timeStep
+    dV = ((drag + stoch)/particleM) * timeStep
     #print("Initial Velocities: " + str(particleV))
     #print("mass of particle (kg): " + str(particleM))
     #print("damping coefficient (SI):" + str(gamma))
     #print("change in velocity: " + str(dV))
     return particleV + dV
 
-def handleCollision(particleV,projectedX,wallParam):
+def handleCollision(boxSize, particleV,projectedX,wallParam):
     # Reflect off a wall
+    b = boxSize
     for i in range(projectedX.shape[0]):
         x,y,z = projectedX[i,:]
         xd,yd,zd = particleV[i,:]
-        while(x < 0 or x > 1 or y < 0 or y > 1):
+        while(x < 0 or x > b or y < 0 or y > b):
             if x < 0:
                 x = abs(x)
                 xd = -xd
-            elif x > 1:
-                x = 2 - x
+            elif x > b:
+                x = 2*b - x
                 xd = -xd
             elif y < 0:
                 y = abs(y)
                 yd = -yd
-            elif y > 1:
-                y = 2 - y
+            elif y > b:
+                y = 2*b - y
                 yd = -yd           
             elif z < 0:
                 z = abs(z)
                 zd = -zd
-            elif z > 1:
-                z = 2 - z
+            elif z > b:
+                z = 2*b - z
                 zd = -zd
-        particleX[i,:] = [x,y,z]
+        projectedX[i,:] = [x,y,z]
         particleV[i,:] = [xd,yd,zd]
-    return particleX,particleV
+    return projectedX,particleV
 
-def randomWalk(eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam):
+def randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam):
     # gamma = 6*pi*eta*a, where
     # eta = fluid viscosity
     # a = effective radius
@@ -84,44 +87,63 @@ def randomWalk(eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam)
     #xHistory = np.zeros((NP,2,totalSteps))
     #vHistory = np.zeros((NP,2,totalSteps))
     
-    particleX,particleV = genIC(kB,NP,particleM,Temp)
-    print(totalEnergy(kB,particleM,particleV,Temp))
-    energies = []
+    particleX,particleV = genIC(boxSize,kB,NP,particleM,Temp)
+    totalEnergy(kB,particleM,particleV,Temp)
+    left = 0
+    right = 0
     for i in range(0,totalSteps):
         if i%10000 == 0:
+            left = 0
+            right = 0
+            for j in range(0,NP):
+                if particleX[j,0] < 0.5*boxSize:
+                    left += 1
+                else:
+                    right += 1
             print(i)
+            print("# on left: " + str(left))
+            print("# on right: " + str(right))
             #plotState(particleX,particleV)
-            energies.append(totalEnergy(kB,particleM,particleV,Temp))
+            #energies.append(totalEnergy(kB,particleM,particleV,Temp))
+            #print(particleX)
         #xHistory[:,:,i] = particleX[:,:]
         #vHistory[:,:,i] = particleV[:,:]
         particleV = updateV(gamma,kB,particleM,particleV,Temp,timeStep)
         #print(totalEnergy(kB,particleM,particleV,Temp))
         projectedX = particleX + particleV * timeStep
         #Handle collisions
-        #particleX,particleV = handleCollision(projectedX,particleV, wallParam)
-        particleX = projectedX
-    print(totalEnergy(kB,particleM,particleV,Temp))    
-
-    return energies
+        particleX, particleV = handleCollision(boxSize, particleV,projectedX, wallParam)
+        #particleX = projectedX
+    
+    totalEnergy(kB,particleM,particleV,Temp)
+    left = 0
+    right = 0
+    for j in range(0,NP):
+        if particleX[j,0] < 0.5*boxSize:
+            left += 1
+        else:
+            right += 1
+    return left, right
 
 #plotState(particleX,particleV)
 kB = 1.4*10**(-23) # Boltzmann's constant
-
-totalTime = 1e-8 # seconds of diffusion
-NP = 10
-Temp = 300 # 300 Kelvin
-particleM = 3 * 10 ** -26 # molecular mass of water in kg
-particleR = 1 * 10 ** -10 # water is a one angstrom radius sphere, yolo
+boxSize = 2e-6 # Our box is 2 mu m right now 
+#totalTime = 1e-8 # seconds of diffusion
+NP = 100
+Temp = 310 # 310 Kelvin
+particleM = (162.0/18.0)*(3 * 10 ** (-26)) # molecular mass of nicotine in kg
+particleR = 3 * 10 ** (-10) # nicotine is a three angstrom radius sphere, yolo
 eta = 1.0 * 10 ** (-6) # viscosity of water in m^2 / s
 wallParam = 0
 gamma = 6*np.pi*eta*particleR
-D = Temp * kB / gamma
-tau = particleR**2 / D
-print("tau = " + str(tau))
-timeStep = tau
-totalSteps = 50000000 #int(totalTime / timeStep)
+#D = Temp * kB / gamma
+#tau = particleR**2 / D
+#print("tau = " + str(tau))
+#print("tv = " + str(particleM/gamma))
+timeStep = 1e-13
+totalSteps = 100000 #int(totalTime / timeStep)
 
-energies = randomWalk(eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam)
+energies = randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam)
 
 #for i in range(timeSteps):
 #    plotState(xHistory[:,:,i],vHistory[:,:,i])
