fcddf6b947e44932c421e03f61c7b88bed5edb4f
Calvin Leung <cleung@hmc.edu>
2015-11-30 23:13:53 +0000

    now we generate ICs externally/plot before/after

--- brownian.py
+++ brownian.py
@@ -28,9 +28,14 @@ def getState(boxSize,particleV, particleX,plot=False):
         plt.axis([0,boxSize,0,boxSize])
         ax.set_zbound(lower=0, upper=boxSize)
         ax.quiver(normalizedX[:,0],normalizedX[:,1],normalizedX[:,2],normalizedV[:,0],normalizedV[:,1],normalizedV[:,2], length = 0.05 * boxSize)
+        ax.set_xlabel('X axis (normal to membrane)')
+        ax.set_ylabel('Y axis (impenetrable walls)')
+        ax.set_zlabel('Z axis (periodic boundary )')
         plt.show()
     left = np.sum(particleX[:,0] < boxSize * 0.5,dtype=int)
-    return left, NP-left #left and right halves 
+    if plot:
+        return left, NP-left,ax #left and right halves
+    return left, NP-left
 
 def totalEnergy(kB,particleM,particleV,Temp):
     NP = particleV.shape[0]
@@ -82,20 +87,18 @@ def membraneCheck(projectedX, wallParam, boxSize, particleR,NP):
     # check if collision with wall in x coordinate
     # AND if y/z coordinates collide with membrane
     # AND prevent sneakarounds
-    return ((projectedX[:,0] < particleR + 0.5*boxSize)*(projectedX[:,0] > -particleR + 0.5*boxSize)) \
-                  * ((np.fmod(projectedX[:,1],modArray) < wallSize) * (np.fmod(projectedX[:,2],modArray) < wallSize) \
-                  + (projectedX[:,1] < 0) + (projectedX[:,1] > boxSize) + (projectedX[:,2] < 0) + (projectedX[:,2] > boxSize))#check if outside the box (so that the particle can't sneak around the side)
+    return ((projectedX[:,0] < (particleR + 0.5*boxSize))*(projectedX[:,0] > (-particleR + 0.5*boxSize))) \
+                  * ((np.fmod(projectedX[:,1],modArray) < wallSize) + (np.fmod(projectedX[:,2],modArray) < wallSize)) \
+                  #+ (projectedX[:,1] < 0) + (projectedX[:,1] > boxSize))#check if outside the box in y axis (so that the particle can't sneak around the side)
 
 def handleCollision(boxSize, particleV, projectedX, wallParam, NP, particleR):
     # Check if x & y coordinates are out of the range (0, boxSize)
     projectedX[:,2] = np.fmod(projectedX[:,2],boxSize)
     toohigh = projectedX > boxSize - particleR
     toolow  = projectedX < 0 + particleR
-    hitMembraneRight = particleV[:,0] >= 0 * membraneCheck(projectedX, wallParam, boxSize, particleR,NP) 
-    hitMembraneLeft = particleV[:,0] < 0 * membraneCheck(projectedX, wallParam, boxSize, particleR,NP)
-    print(hitMembraneRight)
-    print(hitMembraneLeft)
-
+    #hitMembraneRight = (particleV[:,0] >= 0) * membraneCheck(projectedX, wallParam, boxSize, particleR,NP) 
+    #hitMembraneLeft = (particleV[:,0] < 0) * membraneCheck(projectedX, wallParam, boxSize, particleR,NP)
+    
     particleV[toolow] = -particleV[toolow]
     particleV[toohigh]= -particleV[toohigh]
     #particleV[hitMembraneRight,0] = -particleV[hitMembraneRight,0]
@@ -108,16 +111,13 @@ def handleCollision(boxSize, particleV, projectedX, wallParam, NP, particleR):
 
     return projectedX, particleV
 
-def randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam):
+def randomWalk(boxSize,eta,kB,NP,particleM,particleR,particleV0,particleX0,Temp,timeStep,totalSteps,wallParam):
     # gamma = 6*pi*eta*a, where
     # eta = fluid viscosity
     # a = effective radius
     lefts = np.zeros(totalSteps)
     gamma = (6*np.pi*eta*particleR)
-    particleX,particleV = genIC(boxSize,kB,NP,particleM,Temp)
-    left,right = getState(boxSize, particleV, particleX, True)
-    input('Press Enter')
-    totalEnergy(kB,particleM,particleV,Temp)
+    (particleX,particleV) = (particleX0,particleV0)
     
     for i in range(0,totalSteps):
         lefts[i],right = getState(boxSize, particleV, particleX, plot=False)
@@ -132,27 +132,52 @@ def randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wa
         #Update the velocities stochastically
         particleV = updateV(gamma,kB,particleM,particleV,Temp,timeStep)
     totalEnergy(kB,particleM,particleV,Temp)
-    return lefts,particleX,particleV
+    return particleX,particleV
 
 kB = 1.4*10**(-23) # Boltzmann's constant
 boxSize = 2e-6 # Our box is 2 mu m right now 
 #totalTime = 1e-8 # seconds of diffusion
-NP = 10
+NP = 400 # typical nicotine concentration in blood
 Temp = 310 # 310 Kelvin
 particleM = (162.0/18.0)*(3 * 10 ** (-26)) # molecular mass of nicotine in kg
 particleR = 3 * 10 ** (-10) # nicotine is a three angstrom radius sphere, yolo
 eta = 1.0 * 10 ** (-6) # viscosity of water in m^2 / s
 wallParam = [1,1] #[Hole Size (side), hole to wall ratio]
 gamma = 6*np.pi*eta*particleR
-#D = Temp * kB / gamma
-#tau = particleR**2 / D
-#print("tau = " + str(tau))
-#print("tv = " + str(particleM/gamma))
-timeStep = 1e-13
-totalSteps = 1 #int(totalTime / timeStep)
+timeStep = 1e-11
+totalSteps = 100000 #int(totalTime / timeStep)
 
-lefts,particleX,particleV = randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam)
-getState(boxSize,particleV,particleX,True)
+#Generate ICs, get some statistics, run random walk, more statistics
+particleX0,particleV0 = genIC(boxSize,kB,NP,particleM,Temp)
+totalEnergy(kB,particleM,particleV0,Temp)
+getState(boxSize, particleV0, particleX0, True)
+particleX,particleV = randomWalk(boxSize,eta,kB,NP,particleM,particleR,particleV0,particleX0,Temp,timeStep,totalSteps,wallParam)
+totalEnergy(kB,particleM,particleV,Temp)
+getState(boxSize, particleV, particleX, True)
+
+def beforeAfter(particleX0,particleV0,particleX,particleV,boxSize):
+        normalizedX0 = np.copy(particleX0) 
+        normalizedV0 = np.copy(particleV0)
+        normalizedX = np.copy(particleX) 
+        normalizedV = np.copy(particleV)
+        
+        fig = plt.figure(figsize = plt.figaspect(2.))
+        ax1 = fig.add_subplot(2,1,1,projection='3d')
+        plt.axis([0,boxSize,0,boxSize])
+        ax1.set_zbound(lower=0, upper=boxSize)
+        ax1.quiver(normalizedX0[:,0],normalizedX0[:,1],normalizedX0[:,2],normalizedV0[:,0],normalizedV0[:,1],normalizedV0[:,2], length = 0.05 * boxSize)
+        ax1.set_xlabel('X axis (normal to membrane)')
+        ax1.set_ylabel('Y axis (impenetrable walls)')
+        ax1.set_zlabel('Z axis (periodic boundary )')
+
+        ax2 = fig.add_subplot(2,1,2,projection='3d')
+        plt.axis([0,boxSize,0,boxSize])
+        ax2.set_zbound(lower=0, upper=boxSize)
+        ax2.quiver(normalizedX[:,0],normalizedX[:,1],normalizedX[:,2],normalizedV[:,0],normalizedV[:,1],normalizedV[:,2], length = 0.05 * boxSize)
+        ax2.set_xlabel('X axis (normal to membrane)')
+        ax2.set_ylabel('Y axis (impenetrable walls)')
+        ax2.set_zlabel('Z axis (periodic boundary )')
+        plt.show()
     
 #if __name__ == "__main__":
 #    main()
