e16c9e90bacda6b49673c7fd8c4606fa74aa89c5
tmiddlemas <tmiddlemas@hmc.edu>
2015-11-30 00:04:23 -0800

    Added collision detection, still buggy
    I think it's calling too many things as having collided with the membrane.

--- brownian.py
+++ brownian.py
@@ -1,6 +1,6 @@
 # Brownian Motion on the Unit Square
 # Parameters
-# wallParameter - wall spacing
+# wallParameter - wall spacing (given in terms of [side length of hole, wall to hole ratio])
 # particleM - effective particle mass
 # NP - particle number
 # Temp - temperature which dictates thermal distribution (set k_B = 1)
@@ -14,12 +14,7 @@ from numpy import ma
 def genIC(boxSize, kB, NP, particleM, Temp):
     particleX = boxSize*np.random.rand(NP,3)
     particleX[:,0] = 0.5 * particleX[:,0]
-    #speed = np.random.normal(0,particleM/(2 * kB * Temp),(NP,1)) # Boltzmann factor for some speed
-    #angle = np.random.rand(NP,1) * 2 * np.pi
-    #particleV = np.hstack((np.multiply(speed,np.cos(angle)),np.multiply(speed,np.sin(angle))))
-
     particleV = np.random.normal(0,np.sqrt(kB*Temp/particleM),(NP,3))
-    #particleV = np.zeros((NP,3))
     return (particleX, particleV)
 
 def getState(boxSize,particleV, particleX,plot=False):
@@ -28,13 +23,11 @@ def getState(boxSize,particleV, particleX,plot=False):
     if plot:
         normalizedX = np.copy(particleX) 
         normalizedV = np.copy(particleV)
-    
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         plt.axis([0,boxSize,0,boxSize])
         ax.set_zbound(lower=0, upper=boxSize)
-        ax.quiver(normalizedX[:,0],normalizedX[:,1],normalizedX[:,2],normalizedV[:,0],normalizedV[:,1],normalizedV[:,2],
-                  length = 0.05 * boxSize)
+        ax.quiver(normalizedX[:,0],normalizedX[:,1],normalizedX[:,2],normalizedV[:,0],normalizedV[:,1],normalizedV[:,2], length = 0.05 * boxSize)
         plt.show()
     left = np.sum(particleX[:,0] < boxSize * 0.5,dtype=int)
     return left, NP-left #left and right halves 
@@ -81,15 +74,33 @@ def updateV(gamma,kB,particleM,particleV,Temp,timeStep):
 #         particleV[i,:] = [xd,yd,zd]
 #     return projectedX,particleV
 
-def handleCollision(boxSize, particleV, projectedX, wallParam, NP):
-    toohigh = projectedX > boxSize
-    toolow  = projectedX < 0
+def membraneCheck(projectedX, wallParam, boxSize, particleR,NP):
+    holeSize = wallParam[0]
+    ratio = wallParam[1]
+    wallSize = ratio*holeSize
+    modArray = (holeSize + wallSize)*np.ones(NP)
+    return ((projectedX[:,0] < particleR + 0.5*boxSize)*(projectedX[:,0] > -particleR + 0.5*boxSize)) \
+                  * ((np.fmod(projectedX[:,1],modArray) < wallSize) + (np.fmod(projectedX[:,2],modArray) < wallSize) \
+                  + (projectedX[:,1] < 0) + (projectedX[:,1] > boxSize) + (projectedX[:,2] < 0) + (projectedX[:,2] > boxSize))#check if outside the box (so that the particle can't sneak around the side)
+
+def handleCollision(boxSize, particleV, projectedX, wallParam, NP, particleR):
+    toohigh = projectedX > boxSize - particleR
+    toolow  = projectedX < 0 + particleR
+    hitMembraneRight = particleV[:,0] >= 0 * membraneCheck(projectedX, wallParam, boxSize, particleR,NP) 
+    hitMembraneLeft = particleV[:,0] < 0 * membraneCheck(projectedX, wallParam, boxSize, particleR,NP)
+    print(hitMembraneRight)
+    print(hitMembraneLeft)
 
     particleV[toolow] = -particleV[toolow]
     particleV[toohigh]= -particleV[toohigh]
+    #particleV[hitMembraneRight,0] = -particleV[hitMembraneRight,0]
+    #particleV[hitMembraneLeft,0] = -particleV[hitMembraneLeft,0]
 
-    projectedX[toolow] = -projectedX[toolow]
-    projectedX[toohigh] = 2*boxSize - projectedX[toohigh]
+    projectedX[toolow] = -projectedX[toolow] + particleR
+    projectedX[toohigh] = 2*boxSize - projectedX[toohigh] - particleR
+    #projectedX[hitMembraneRight,0] = 2*(0.5*boxSize - particleR) - projectedX[hitMembraneRight,0]
+    #projectedX[hitMembraneLeft,0] = 2*(0.5*boxSize + particleR) - projectedX[hitMembraneLeft,0] 
+
     return projectedX, particleV
 
 def randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam):
@@ -97,9 +108,7 @@ def randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wa
     # eta = fluid viscosity
     # a = effective radius
     lefts = np.zeros(totalSteps)
-    gamma = (6*np.pi*eta*particleR)   
-    #xHistory = np.zeros((NP,2,totalSteps))
-    #vHistory = np.zeros((NP,2,totalSteps))
+    gamma = (6*np.pi*eta*particleR)
     particleX,particleV = genIC(boxSize,kB,NP,particleM,Temp)
     left,right = getState(boxSize, particleV, particleX, True)
     input('Press Enter')
@@ -111,29 +120,31 @@ def randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wa
             print("Timestep: "   +  str(i))
             print("# on left: "  + str(lefts[i]))
             print("# on right: " + str(right))
-        particleV = updateV(gamma,kB,particleM,particleV,Temp,timeStep)
+        #Step forward
         projectedX = particleX + particleV * timeStep
         #Handle collisions
-        particleX, particleV = handleCollision(boxSize, particleV,projectedX, wallParam, NP)
+        particleX, particleV = handleCollision(boxSize, particleV,projectedX, wallParam, NP, particleR)
+        #Update the velocities stochastically
+        particleV = updateV(gamma,kB,particleM,particleV,Temp,timeStep)
     totalEnergy(kB,particleM,particleV,Temp)
     return lefts,particleX,particleV
 
 kB = 1.4*10**(-23) # Boltzmann's constant
 boxSize = 2e-6 # Our box is 2 mu m right now 
 #totalTime = 1e-8 # seconds of diffusion
-NP = 100
+NP = 10
 Temp = 310 # 310 Kelvin
 particleM = (162.0/18.0)*(3 * 10 ** (-26)) # molecular mass of nicotine in kg
 particleR = 3 * 10 ** (-10) # nicotine is a three angstrom radius sphere, yolo
 eta = 1.0 * 10 ** (-6) # viscosity of water in m^2 / s
-wallParam = 0
+wallParam = [1,1] #[Hole Size (side), hole to wall ratio]
 gamma = 6*np.pi*eta*particleR
 #D = Temp * kB / gamma
 #tau = particleR**2 / D
 #print("tau = " + str(tau))
 #print("tv = " + str(particleM/gamma))
 timeStep = 1e-13
-totalSteps = 10000000 #int(totalTime / timeStep)
+totalSteps = 1 #int(totalTime / timeStep)
 
 lefts,particleX,particleV = randomWalk(boxSize,eta,kB,NP,particleM,particleR,Temp,timeStep,totalSteps,wallParam)
 getState(boxSize,particleV,particleX,True)
